d68429a40acbce82631895abcf1275bd58c70ece
WOOD SAMUEL R <wosa@mac06-cs.ms.okstate.edu>
2017-10-07 12:07:52 -0500

    Initial Commit

---
+++ T02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  T02
+//
+//  Created by WOOD SAMUEL R on 10/7/17.
+//  Copyright © 2017 Brightest Orange. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
