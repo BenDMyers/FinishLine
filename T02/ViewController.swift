7e47abf38f16db39a1038907fdb8a7aefb0b0f37
wosa <samuel.r.wood@okstate.edu>
2017-10-31 11:50:15 -0500

    committing schedule1

--- T02/ViewController.swift
+++ T02/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+ 
+    var outTime = NSDate()
+    var inTime = NSDate()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
