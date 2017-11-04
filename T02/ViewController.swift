e6cd6fe4beed56135a89d89c1b3d8b5563f2a51d
MYERS BENJAMIN D <bemy@mac03-cs.ms.okstate.edu>
2017-11-03 20:53:13 -0500

    New homescreen buttons

--- T02/ViewController.swift
+++ T02/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let red = UIColor(red: 0.8, green: 0.0, blue: 0.0, alpha: 1.0)
+    let yellow = UIColor(red: 1.0, green: 1.0, blue: 0.0, alpha: 1.0)
+    let blue = UIColor(red: 0.0, green: 0.0, blue: 1.0, alpha: 1.0)
+    let green = UIColor(red: 0.42, green: 0.66, blue: 0.31, alpha: 1.0)
  
     var outTime = NSDate()
     var inTime = NSDate()
